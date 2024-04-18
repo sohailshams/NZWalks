2277c67d62e594b08b9b016b4a8a7475f8d37124
sohailshams <sohailshamas@yahoo.com>
2024-04-18 23:30:10 +0100

    Add project files.

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+using NZWalks.API.DTOs;
+using NZWalks.API.Models;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext dbContext;
+
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            // Get Data from DB - model
+            var regions = dbContext.Regions.ToList();
+            
+            // Map model to DTOs
+            var regionDtos = new List<RegionDTO>();
+            foreach (var region in regions)
+            {
+                regionDtos.Add(new RegionDTO() 
+                { 
+                    Id = region.Id, 
+                    Code = region.Code,
+                    Name = region.Name, 
+                    RegionImageUrl = region.RegionImageUrl 
+                });
+            }
+
+            // return DTOs
+            return Ok(regionDtos);
+        }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            // Get Data from DB - model
+
+            //var region = dbContext.Regions.Find(id);
+            var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            // Map model to DTOs
+            var regionDto = new RegionDTO
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                RegionImageUrl = region.RegionImageUrl
+            };
+
+            // return DTO
+            return Ok(regionDto);
+        }
+
+        [HttpPost]
+        public IActionResult AddReagion([FromBody] AddRegionDto addRegion)
+        {
+            // Convert DTO to model
+            var regionModel = new Region
+            {
+                Code = addRegion.Code,
+                Name = addRegion.Name,
+                RegionImageUrl = addRegion.RegionImageUrl
+            };
+
+            // Use model to create region in DB
+            dbContext.Regions.Add(regionModel);
+            dbContext.SaveChanges();
+
+            // Map model to DTOs
+            var regionDto = new RegionDTO
+            {
+                Id = regionModel.Id,
+                Code = regionModel.Code,
+                Name = regionModel.Name,
+                RegionImageUrl = regionModel.RegionImageUrl
+            };
+
+            return CreatedAtAction(nameof(GetById), new { id = regionDto.Id }, regionDto);
+        }
+    }
+}
