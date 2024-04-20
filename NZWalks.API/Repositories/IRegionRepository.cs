b1fe2a93baf15610948e3ccddfe4b6898d6212eb
sohailshams <sohailshamas@yahoo.com>
2024-04-20 23:15:36 +0100

    Ceated IRegionRepository interface and RegionRespository.

---
+++ NZWalks.API/Repositories/IRegionRepository.cs
@@ -0,0 +1,9 @@
+﻿using NZWalks.API.Models;
+
+namespace NZWalks.API.Repositories
+{
+    public interface IRegionRepository
+    {
+      Task<List<Region>>  GetAllAsync();
+    }
+}
---
+++ NZWalks.API/Repositories/RegionRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models;
+
+namespace NZWalks.API.Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private readonly NZWalksDbContext dbContext;
+        public RegionRepository(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public NZWalksDbContext DbContext { get; }
+
+        public async Task<List<Region>> GetAllAsync()
+        {
+            var regions = await dbContext.Regions.ToListAsync();
+            return regions;
+        }
+    }
+}
